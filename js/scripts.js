178d49527fa3cc13f0588b15ffb59e66f5f616a6
E-FewellHoward <31193920+E-FewellHoward@users.noreply.github.com>
2017-09-19 21:54:24 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,17 @@ $(document).ready(function(){
     $('[data-toggle="popover"]').popover();   
 });
 
+$(document).ready(function(){
+    $("#Connect").click(function(){
+        $("#ConnectModal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#Login").click(function(){
+        $("#loginModal").modal();
+    });
+});
+
+
+
